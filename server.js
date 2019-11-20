d5b37f0079bddf2b4f30e222eaa8a11d084536cf
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2019-11-20 16:26:49 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/start', (req, res) => {
+    res.send('Hello App');
+});
+
+const port = 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
