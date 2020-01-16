1716deafaf2b72d358d3bb222829a35edd1b7b17
tudorionutelian <you@example.comionuteliantudor@gmail.com>
2020-01-16 19:17:12 +0000

    Split components

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -3,14 +3,26 @@ import React, {Component} from 'react';
 import Utilizatori from './utilizatori/utilizatori';
 
 class Body extends Component{
+  constructor(){
+    super();
+    this.state = {
+      urlStatus: ""
+    };
+  }
+
+  componentDidMount(){
+    let url = window.location.pathname;
+    this.setState({urlStatus:url});
+  }
     
   render(){
+    console.log(this.state.urlStatus);
     return(
       <div>        
         <Utilizatori/>
       </div>
     )
-  }
+  }o
 
 }
 
